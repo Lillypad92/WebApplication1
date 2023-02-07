90f064cb65359072862a7befc5c354c293186e1a
Linda F <linda.forslund92@outlook.com>
2023-02-07 16:35:35 +0100

    Images
    Here I have added some images from backend to frontend.

--- WebApplication1/Pages/Animals.cshtml.cs
+++ WebApplication1/Pages/Animals.cshtml.cs
@@ -3,8 +3,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication1.Pages
 {
+    public class Animal
+    {
+        public string ImageSrc { get; set; }
+        public string Name { get; set; }
+    }
+
     public class AnimalsModel : PageModel
     {
+
+        public List<Animal> Animals { get; set; }
+
+        public AnimalsModel() 
+        {
+            Animals = new List<Animal>()
+            {
+                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Blåsfisk"},
+                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Katt"},
+                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Hund"},
+                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Mask"}
+            };
+        }
+
         public void OnGet()
         {
         }
