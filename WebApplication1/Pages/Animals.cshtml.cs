474e003c48c6200fa10020c4288b372204945daf
Linda F <linda.forslund92@outlook.com>
2023-02-17 13:44:22 +0100

    CSS/Image
    I manage to add some CSS to make the images in a row and next to each other.

--- WebApplication1/Pages/Animals.cshtml.cs
+++ WebApplication1/Pages/Animals.cshtml.cs
@@ -16,16 +16,16 @@ namespace WebApplication1.Pages
 
         public List<Animal> Animals { get; set; }
 
-        public AnimalsModel() 
+        public AnimalsModel()
         {
             Animals = new List<Animal>()
             {
                 new Animal{ImageSrc = "images/computer-code.jpg", Name = "Blåsfisk", Age = 1},
                 new Animal{ImageSrc = "images/computer-code.jpg", Name = "Katt", Age = 2},
-                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Hund", Age = 3},
-                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Mask", Age = 4},
-                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Blåmes", Age = 5},
-                new Animal{ImageSrc = "images/computer-code.jpg", Name = "Kråka", Age = 6}
+                //new Animal{ImageSrc = "images/computer-code.jpg", Name = "Hund", Age = 3},
+                //new Animal{ImageSrc = "images/computer-code.jpg", Name = "Mask", Age = 4},
+                //new Animal{ImageSrc = "images/computer-code.jpg", Name = "Blåmes", Age = 5},
+                //new Animal{ImageSrc = "images/computer-code.jpg", Name = "Kråka", Age = 6},
             };
         }
 
@@ -35,4 +35,3 @@ namespace WebApplication1.Pages
     }
 }
 
-CacheProfile#
